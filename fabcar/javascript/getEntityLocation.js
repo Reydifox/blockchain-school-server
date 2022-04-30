be3eec31f2c412bac47865b0983422a052ed26ab
Roman3579 <xglvac@stuba.sk>
2022-04-30 12:59:43 +0200

    feat: add function to retrieve entity location
    Also added some dependencies

---
+++ fabcar/javascript/getEntityLocation.js
@@ -0,0 +1,17 @@
+const fs = require('fs')
+const raw_config_file = fs.readFileSync('../config/entity_config.json')
+const config = JSON.parse(raw_config_file)
+
+function getEntityLocation(entity_name){
+    if(config.ledger.includes(entity_name)){
+        return "ledger"
+    } 
+    else if(config.db.includes(entity_name)){
+        return "db";
+    }
+    else {
+        throw `Entity ${entity_name} does not exist in entity_config file.`
+    }
+}
+
+module.exports = getEntityLocation

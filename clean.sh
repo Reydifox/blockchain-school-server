b0f3eaf519570e9997f516d91d9015a01c52d3ee
Reydifox <reydifox@gmail.com>
2022-05-01 19:45:56 +0200

    fix: clean script

--- clean.sh
+++ clean.sh
@@ -11,6 +11,8 @@ cd ../config
 sudo npm install
 echo "Cleaning node server build.."
 cd ../../web
+rm -rf node_modules
+rm package-lock.json
 sudo npm install bcrypt --unsafe-perms
 sudo npm install
 echo "Done"

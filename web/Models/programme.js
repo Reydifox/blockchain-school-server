126712366fe05ae5c9187d999577866ec55371ac
Marek Drab <marek.drablp@gmail.com>
2022-05-09 18:49:59 +0200

    /programme added - put method wip

---
+++ web/Models/programme.js
@@ -0,0 +1,38 @@
+const auth = require('../Auth/auth')
+const infrastructure = require('../../fabcar/api/api.js');
+const helpers = require('../Helpers/helpers');
+
+module.exports = {
+    getAllProgramme: async function (req) {
+        let result = await infrastructure.getAllEntities(auth.get_bearer(req),'study_programme')
+        return result.result
+    },
+    addProgramme: async function (req) {
+        let programme = {
+            name: req.body.name,
+            entity_name:"study_programme",
+            acronym:req.body.acronym
+        }
+        let result = await infrastructure.putEntity(auth.get_bearer(req), programme)
+        programme.id = result.id
+        return programme
+    },
+    getProgrammeById: async function (req) {
+        let result = await infrastructure.getEntity(auth.get_bearer(req), req.params.id)
+        return result
+    },
+    updateProgramme: async function (req) {
+        let programme = {
+            _id: req.params.id,
+            name: req.body.name,
+            acronym:req.body.acronym
+        } 
+        
+        let result = await infrastructure.updateEntity(auth.get_bearer(req),programme)
+        return result
+    },
+    deleteProgramme: async function (req) {
+        let result = await infrastructure.deleteEntity(auth.get_bearer(req),req.params.id)
+        return result
+    }
+}
--- web/Routes/programme.js
+++ web/Routes/programme.js
@@ -1,27 +1,40 @@
 const express = require('express')
 const router = express.Router()
-const programme = {
-  id : 1,
-  name : "Aplikovana informatika",
-  acronym : "API",
-}
+const programme = require('../Models/programme')
 
 router.route('/')
   .get(async (req, res) => {
-    res.json([programme, programme])
+    let result = await programme.getAllProgramme(req)
+    res.json(result)
   })
   .post(async (req, res) => {
-    res.json(req.body)
+    let result = await programme.addProgramme(req)
+    res.json(result)
   })
 
 router.route('/:id')
   .get(async (req, res) => {
-    res.json(programme)
+    try {
+      let result = await programme.getProgrammeById(req)
+      res.json(result)
+    } catch (e) {
+      res.json(e)
+    }
   })
   .put(async (req, res) => {
-    res.json(req.body)
+    try {
+      let result = await programme.updateProgramme(req)
+      res.json(result)
+    } catch (e) {
+      res.json(e)
+    }
   })
   .delete(async (req, res) => {
-    res.sendStatus(204)
+    try {
+      let result = await programme.deleteProgramme(req)
+      res.json(result)
+    } catch (e) {
+      res.json(e)
+    }
   })
 module.exports = router

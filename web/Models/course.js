e24b63d89a9c8332d340fdda59bec7f744bd89e1
peter <pandrejko12@gmail.com>
2022-05-10 16:43:43 +0200

    addCourse and getCourseById implemented fully

--- web/Models/course.js
+++ web/Models/course.js
@@ -9,30 +9,41 @@ module.exports = {
     return courses.result
   },
   getCourseById: async function (req) {
-    let course = await infrastructure.getEntity(auth.get_bearer(req), req.params.id)
-    if ('lecturers' in course) {
-      //TODO
+    let course = await infrastructure.getEntity(auth.get_bearer(req), req.params.id);
+    if ('garant_id' in course) {
+      let garant = await infrastructure.getEntity(auth.get_bearer(req), course.garant_id)
+      course.garant = garant;
     }
-    if ('prerequisites' in course) {
-      //TODO
+    console.log(course.garant);
+
+    if ('prerequisite_course_id' in course) {
+      if(Array.isArray(course.prerequisite_course_id) && course.prerequisite_course_id.length) {
+        let coursePrerequisites = []
+        console.log(course.prerequisite_course_id);
+        for (let item in course.prerequisite_course_id) {
+          let courseId = course.prerequisite_course_id[item];
+          let tmp_course = await infrastructure.getEntity(auth.get_bearer(req), courseId);
+          console.log(tmp_course);
+          coursePrerequisites.push(tmp_course);
+        }
+      course.prerequisite_course = coursePrerequisites;
+      console.log(course.prerequisite_course);
+      }
     }
-    return course
+    return course;
   },
   addCourse: async function (req) {
-    if ('prerequisities_course_ids' in req.body){
-      //TODO
-    }
     let course = {
       garant_id: req.body.garant_id,
       name: req.body.name,
       acronym: req.body.acronym,
       description: req.body.description,
       trimester: req.body.trimester,
-      prerequisite_course_ids: req.body.prerequisite_course_ids,
-    }
-    course.entity_name = 'course'
-    let result = await infrastructure.putEntity('admin', course)
-    return course
+      prerequisite_course_id: req.body.prerequisite_course_id,
+    };
+    course.entity_name = 'course';
+    let result = await infrastructure.putEntity('admin', course);
+    return course;
   },
 
 };

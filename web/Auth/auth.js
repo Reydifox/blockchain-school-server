0342df8f7b9c8527938e3da1383b33c64b70e95c
Marek Drab <marek.drablp@gmail.com>
2022-05-14 17:31:34 +0200

    login role fix

--- web/Auth/auth.js
+++ web/Auth/auth.js
@@ -1,4 +1,3 @@
-const auth = require('../Auth/auth')
 const infrastructure = require('../../fabcar/api/api.js');
 const helpers = require('../Helpers/helpers');
 const { updateUser } = require('../Models/user');
@@ -20,13 +19,10 @@ module.exports = {
         const result = await infrastructure.updateUser(req);
         return result;
     },
-    get_userRole: async function(user_id){
-        let role = await infrastructure.getEntity(user_id.split('_')[0], user_id)
-        let system_credibilities = await helpers.getSystemCredibilities(role.system_credibility_id)
-        let returnValue = system_credibilities
-        return returnValue
-    }
-    
+    get_userRole: async function(user_role_id){
+        let role = await infrastructure.getEntity('admin', user_role_id)
+        return role
+    },
     // getUserById: async function (req) {
     //     let user = await infrastructure.getEntity(auth.get_bearer(req), req.params.id)
     //     if ('address_id' in user) {
